d8741510055eec63e8d8bc8e2f4dd01d426e507e
LgCookie <lgc2333@126.com>
2025-05-08 19:52:06 +0800

    up

--- packages/yakumo-commit/src/index.ts
+++ packages/yakumo-commit/src/index.ts
@@ -1,6 +1,7 @@
 import fs from 'node:fs'
 import pathLib from 'node:path'
 
+import type Logger from 'reggol'
 import z from 'schemastery'
 import { simpleGit } from 'simple-git'
 import type { Context } from 'yakumo'
@@ -20,57 +21,65 @@ export const Config: z<Config> = z.object({
   retries: z.number().default(3),
 })
 
+// eslint-disable-next-line antfu/top-level-function
+const mapToErr = (e: unknown): Error => {
+  return e instanceof Error ? e : new Error(String(e))
+}
+
 export function apply(ctx: Context, config: Config) {
   const logger = ctx.logger('yakumo-commit')
 
   const action = async (
+    logger: Logger,
     cwd: string,
     path: string,
     msg: string,
     push: boolean,
-    retries: number = 3,
   ) => {
-    const logger = ctx.logger(path)
+    const joinedPath = pathLib.join(cwd, path)
 
-    const realAction = async () => {
-      const joinedPath = pathLib.join(cwd, path)
-
-      if (!fs.existsSync(pathLib.join(joinedPath, '.git'))) {
-        logger.warn(`skipped`)
-        return
-      }
-
-      const git = simpleGit({ baseDir: joinedPath })
-
-      let status = await git.status()
-
-      if (status.isClean()) {
-        logger.info(`no changes`)
-      } else {
-        logger.info(`committing...`)
-        await git.add('.').commit(msg)
-        status = await git.status()
-      }
-
-      // has not pushed commits
-      if (push) {
-        if (status.ahead) {
-          logger.info(`pushing...`)
-          await git.push()
-        } else {
-          logger.info(`no staged commits`)
-        }
-      }
+    if (!fs.existsSync(pathLib.join(joinedPath, '.git'))) {
+      logger.warn(`skipped`)
+      return
     }
 
-    for (let i = 0; i < retries; i++) {
-      try {
-        return await realAction()
-      } catch (e) {
-        logger.warn(`${e} (tried ${i} times)`)
+    const git = simpleGit({ baseDir: joinedPath })
+
+    let status = await git.status()
+
+    if (status.isClean()) {
+      logger.info(`no changes`)
+    } else {
+      logger.info(`committing...`)
+      await git.add('.').commit(msg)
+      status = await git.status()
+    }
+
+    if (push) {
+      if (status.ahead) {
+        logger.info(`pushing...`)
+        await git.push()
+      } else {
+        logger.info(`no staged commits`)
       }
     }
   }
+
+  const wrapRetry =
+    <A extends any[], R>(
+      fn: (...args: A) => R,
+      retries: number,
+      { logger: localLogger }: { logger?: Logger } = {},
+    ) =>
+    async (...args: A) => {
+      for (let i = 0; i < retries; i++) {
+        try {
+          return await fn(...args)
+        } catch (e) {
+          ;(localLogger ?? logger).warn(`${e} (tried ${i} times)`)
+        }
+      }
+    }
 
   ctx.register(
     'commit',
@@ -83,20 +92,22 @@ export function apply(ctx: Context, config: Config) {
       const retries: number = ctx.yakumo.argv.retries ?? config.retries
 
       const results = await Promise.all(
-        paths.map((path) =>
-          action(ctx.yakumo.cwd, path, msg, push, retries).catch((e) =>
-            e instanceof Error ? e : new Error(e),
-          ),
-        ),
+        paths.map((path) => {
+          const logger = ctx.logger(path)
+          const f = wrapRetry(action, retries, { logger })
+          return f(logger, ctx.yakumo.cwd, path, msg, push).catch(mapToErr)
+        }),
       )
 
-      const hasError = results.some((e) => e instanceof Error)
+      const hasError = results.some(Boolean)
       if (hasError) {
         logger.error('error occurred, will not continue committing root workspace')
         return
       }
 
-      await action(ctx.yakumo.cwd, '/', msg, push, retries)
+      const pathLogger = ctx.logger('/')
+      const f = wrapRetry(action, retries, { logger: pathLogger })
+      await f(pathLogger, ctx.yakumo.cwd, '/', msg, push)
     },
     {
       boolean: ['push'],
